b4dabb2613562c399ecba79821414ab706bfaa3c
John Green <jpgreen@Johns-iMac.local>
2018-07-21 04:24:40 -0400

    part one complete

--- ChatBot/ConversationDataSource.swift
+++ ChatBot/ConversationDataSource.swift
@@ -2,21 +2,30 @@ import Foundation
 class ConversationDataSource {
     
     /// The number of Messages in the conversation
-    let messageCount = 0
+    var messageCount = 0
+    
+    // Array to store messages
+    var messages = [Message]()
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        messageCount += 1
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        messageCount += 1
+        let message = Message(date: Date(), text: answer, type: .answer)
+        messages.append(message)
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        return messages[index]
     }
 }
